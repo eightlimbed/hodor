ed71bec9b682991bcee957663781570536268be4
Lee Gaines <eightlimbed@gmail.com>
2018-02-03 15:24:33 -0800

    updates readme and adds comments to python script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## hodor
+This project came from a challenge to try to manipulate the vote counts for
+online polls. `level_0.py` is a python script that sends a POST request to a
+site along with an ID and number of votes you want to submit for that ID.
--- level_0.py
+++ level_0.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 """
-This is my solution to the Hold the Door Challenge - Level 0
+Hold the Door Challenge - Level 0
 This script will cast an aritrary number of votes for an arbitrary id.
 Just change `eyed` and `votes` to the id and number of votes you want to sumbit
 """
 import requests
 
-eyed = 247
-votes = 124
+eyed = 247      # the user's ID
+votes = 124     # number of votes you want to cast
 
 url = 'http://158.69.76.135/level0.php'
 payload = {'id': eyed, 'holdthedoor': 'Submit'}
